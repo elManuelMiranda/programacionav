6b3c60c810e4a5ee3ea566e60c8167b7157cb00d
Manuel Miranda <88564723+elManuelMiranda@users.noreply.github.com>
2022-09-15 18:46:09 -0600

    Act. 4
    Genera un login con estilos a través de less

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="public/css/main.css">
+    <title>Panel</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <fieldset action="">
+            <form action="">
+                <legend>
+                    Panel access
+                </legend>
+
+                <label>Email</label>
+                <input type="email">
+
+                <label>Password</label>
+                <input type="password">
+
+                <button type="submit">Log in</button>
+            </form>
+        </fieldset>
+
+    </div>
+</body>
+
+</html>
